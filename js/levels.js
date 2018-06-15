b212a6e921aa2b57fd7fd152f917c776ce1318b8
Jan Hořava <czechinthemail@gmail.com>
2018-06-14 21:28:11 -0700

    started fortress.json

--- js/levels.js
+++ js/levels.js
@@ -4,6 +4,7 @@ var multiverselevels = {
   'citymaze': 'City Maze',
   'desertstreetmarket': 'Desert Street Market',
   'dreams': 'Dreams',
+  'fortress': 'Fortress',
   'techdemo': 'Tech Demo',
   'trains': 'Trains',
 };
