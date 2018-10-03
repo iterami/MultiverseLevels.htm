1c8de529dbece47d42d84af93f7c55bfdf786822
Jan Hořava <czechinthemail@gmail.com>
2018-10-03 14:55:48 -0700

    removed forest, adjusted prefabs

--- js/levels.js
+++ js/levels.js
@@ -6,7 +6,6 @@ window.multiversecharacters = {
 window.multiverselevels = {
   'citymaze': 'City Maze',
   'desertstreetmarket': 'Desert Street Market',
-  'forest': 'Forest',
   'labs': 'Labs',
   'platforms': 'Platforms',
   'subway': 'Subway Station',
