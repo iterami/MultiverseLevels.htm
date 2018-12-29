2045b0bd4f3d3448997971a126a2ce62da55ec7e
Jan Hořava <czechinthemail@gmail.com>
2018-12-28 16:32:04 -0800

    removed removed levels

--- js/levels.js
+++ js/levels.js
@@ -4,10 +4,6 @@ window.multiversecharacters = {
   'character-default': 'Default',
 };
 window.multiverselevels = {
-  'citymaze': 'City Maze',
-  'desertstreetmarket': 'Desert Street Market',
   'labs': 'Labs',
   'platforms': 'Platforms',
-  'subway': 'Subway Station',
-  'sunset': 'Sunset',
 };
